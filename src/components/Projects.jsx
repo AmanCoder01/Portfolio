b3e825f059267e38ad96dbd8a680b9afc1b03829
Aman Maurya <114045665+AmanCoder01@users.noreply.github.com>
2024-03-09 11:45:40 +0530

    first commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,7 +16,8 @@ const work = [
     {
         title: "Blog App",
         description: "A personal blog is an online platform where individuals share their thoughts, experiences, and interests with a global audience.  ",
-        image: "https://images.unsplash.com/photo-1614332287897-cdc485fa562d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        image: "./showCase3.png",
+        url:"https://blog-app-ten-sage.vercel.app/"
     },
 
 ]
